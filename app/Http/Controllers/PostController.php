4a6a3b2a6296ccf16d6b66ce4e56781127251bb1
AyakaTakei <ayaka.takei82@gmail.com>
2021-09-12 19:46:46 +0900

    詳細画面追加

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return $post->get();
+    }
+/**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+    public function show(Post $post)
+    {
+    return view('show')->with(['post' => $post]);
+    }
+}
