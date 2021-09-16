f47f5b6c4caf80589cfc7ee9883ec932196cba87
AyakaTakei <ayaka.takei82@gmail.com>
2021-09-16 09:40:48 +0900

    ブログ投稿作成処理実行

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,7 @@ Route::get('/posts/', 'PostController@index');
 
 Route::get('/posts/create', 'PostController@create');
 
+Route::post('/posts', 'PostController@store');
+
 Route::get('/posts/{post}', 'PostController@show');
+
