92c024a369a2c2bed80b34c82c25639c4e131c6a
AyakaTakei <ayaka.takei82@gmail.com>
2021-09-17 18:08:28 +0900

    8-5ブログ投稿の編集

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::post('/posts', 'PostController@store');
 
 Route::get('/posts/{post}', 'PostController@show');
 
+Route::get('/posts/{post}/edit', 'PostController@edit');
+
+Route::put('/posts/{post}', 'PostController@update');
